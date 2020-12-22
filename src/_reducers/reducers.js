7de81b2ac3bf2bf1526f89e8266410f3a78a8278
강혜지 <hyeji.kang@softwareinlife.com>
2020-12-22 10:32:05 +0900

    테스트 라이브러리 추가

--- src/_reducers/reducers.js
+++ src/_reducers/reducers.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
-import auth from './auth_reducer'
+import auth from './auth_reducer';
+import common from './common_reducer';
 
 const rootReducer=combineReducers({
     auth,
+    common,
 });
 
 export default rootReducer
